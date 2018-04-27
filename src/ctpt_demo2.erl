ef216807627deaae1217b051ff04791eb5a1b340
Pierre Fenoll <pierrefenoll@gmail.com>
2018-04-27 15:37:59 +0200

    harder_inputs: more Erlangy demo2

--- src/ctpt_demo2.erl
+++ src/ctpt_demo2.erl
@@ -6,13 +6,12 @@
 
 -spec f(string()) -> boolean().
 f(L) when is_list(L) ->
-    Len = length(L),
-    case Len > 0 andalso lists:nth(1,L) of
-        "H" ->
-            case Len > 1 andalso lists:nth(2,L) of
-                "i" ->
-                    case Len > 2 andalso lists:nth(3,L) of
-                        "!" -> ?HERE, false;
+    case L of
+        "H"++_ ->
+            case L of
+                "Hi"++_ ->
+                    case L of
+                        "Hi!"++_ -> ?HERE, false;
                         _ -> ?HERE, true
                     end;
                 _ -> ?HERE, true
